88416a0970e56b6480316a049678331a6869fcd1
Gleb Mozheiko <oxied.github@gmail.com>
2016-07-20 19:14:10 +0300

    Initial app

---
+++ app/src/main/java/com/oxied/tools/package_usage_statistics/MainActivity.java
@@ -0,0 +1,14 @@
+package com.oxied.tools.package_usage_statistics;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
